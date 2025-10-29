5234ad5f983502b1b4dfc58d5618cd8bf21a2f04
Adolfo Gante <adolfogante@gmail.com>
2025-10-29 05:42:26 -0700

    fixed data table sizing

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -21,6 +21,66 @@ async function getData(): Promise<URLData[]> {
 	return data
 }
 
+{/*
+const dp: URLData[] =
+	[
+		{
+			"created_at": "10/25/2025",
+			"full": "https://duckduckgo.com/",
+			"short": "https://adoante.com/2",
+			"clicks": 2
+		},
+		{
+			"created_at": "10/25/2025",
+			"full": "https://supabase.com/",
+			"short": "https://adoante.com/3",
+			"clicks": 1
+		},
+		{
+			"created_at": "10/28/2025",
+			"full": "https://myalmacoffee.com/products/agua-fria",
+			"short": "https://adoante.com/7",
+			"clicks": 0
+		},
+		{
+			"created_at": "10/29/2025",
+			"full": "https://appraise-benchmark.com/",
+			"short": "https://adoante.com/8",
+			"clicks": 0
+		},
+		{
+			"created_at": "10/23/2025",
+			"full": "https://www.adolfogante.com/",
+			"short": "https://adoante.com/1",
+			"clicks": 20
+		},
+		{
+			"created_at": "10/29/2025",
+			"full": "https://www.adolfogante.com/blog",
+			"short": "https://adoante.com/9",
+			"clicks": 1
+		},
+		{
+			"created_at": "10/29/2025",
+			"full": "https://www.adolfogante.com/contact",
+			"short": "https://adoante.com/A",
+			"clicks": 1
+		},
+		{
+			"created_at": "10/29/2025",
+			"full": "https://www.adolfogante.com/work",
+			"short": "https://adoante.com/B",
+			"clicks": 1
+		},
+		{
+			"created_at": "10/29/2025",
+			"full": "https://www.adolfogante.com/about",
+			"short": "https://adoante.com/C",
+			"clicks": 1
+		}
+	]
+*/}
+
 export default function Dashboard() {
 	const [data, setData] = useState<URLData[]>([])
 	const [loading, setLoading] = useState(true)
@@ -48,9 +108,8 @@ export default function Dashboard() {
 			</div>
 
 			{loading && <p className="text-gray-400">Loading URLs...</p>}
-			{!loading && <DataTable columns={columns} data={data} />}
+			{!loading && <div className="w-full"><DataTable columns={columns} data={data} /></div>}
 			{error && <p className="text-red-400">Error: {error}</p>}
 		</main>
 	)
 }
-
